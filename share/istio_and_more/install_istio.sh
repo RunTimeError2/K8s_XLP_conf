f27cc2329d2f156dddb1c208f4bf61c1373b07ba
RunTimeError2 <songjiahao980530@126.com>
2019-08-01 18:42:10 +0800

    Deleted meshpolicies... default

--- share/istio_and_more/install_istio.sh
+++ share/istio_and_more/install_istio.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 for i in istio-1.2.2/install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
-kubectl apply -f istio-1.2.2/install/kubernetes/istio-demo-auth.yaml
+#kubectl apply -f istio-1.2.2/install/kubernetes/istio-demo-auth.yaml
+kubectl apply -f istio-demo.yaml
+echo 'Waiting for Istio to initialize...'
+sleep 20s
+kubectl delete meshpolicies.authentication.istio.io default
+
 kubectl get pods -n istio-system
 kubectl get svc -n istio-system
