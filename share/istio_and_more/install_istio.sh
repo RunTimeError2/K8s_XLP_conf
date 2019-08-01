cd0c08592a438b030a9dc63a22eccaa8e7f4211c
RunTimeError2 <songjiahao980530@126.com>
2019-08-01 11:01:38 +0800

    Applied istio-demo-auth.yaml

--- share/istio_and_more/install_istio.sh
+++ share/istio_and_more/install_istio.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 for i in istio-1.2.2/install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
-kubectl apply -f istio-1.2.2/install/kubernetes/istio-demo.yaml
-#kubectl apply -f install/kubernetes/istio-demo-auth.yaml
+kubectl apply -f istio-1.2.2/install/kubernetes/istio-demo-auth.yaml
 kubectl get pods -n istio-system
 kubectl get svc -n istio-system
